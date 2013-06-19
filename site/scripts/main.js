2d73e3e21969467949c0980055edc52add3980c2
Simon Smith <simon@interlopers.net>
2013-06-19 22:22:11 +0100

    Improve main.js example

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -20,29 +20,16 @@ require.config({
 define(['jquery', 'handlebars', 'domready'], function() {});
 
 
+// If you'd like to load some core modules that interact with the page then you can
+// use an alternative style
 /*
-If you'd like to load some core modules that interact with the page then you can
-use an alternative style
-
 define(function(require) {
-    var $          = require('jquery');
-    var Handlebars = require('handlebars');
-    var domReady   = require('domready');
-
-    var Navigation = require('modules/navigation');
-
-    return function() {
-        domReady(function() {
-            new Navigation($('#container'));
-        });
-    }
+    var $        = require('jquery');
+    var domReady = require('domready');
+                   require('handlebars');
+    
+    domReady(function() {
+        // Init common module code here
+    });
 });
-
-// Create main.js
-require(['core'], function(core) {
-    core();
-});
-
-// index.html uses main isntead of core
-require(['main']);
 */
